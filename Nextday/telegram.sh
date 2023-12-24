d2f81016bdfdc82ae04797a02ef83375a87a1a2d
Kawamura Shingo <36791900+kawamurashingo@users.noreply.github.com>
2023-12-24 12:27:33 +0900

    Update telegram.sh

--- Nextday/telegram.sh
+++ Nextday/telegram.sh
@@ -1,93 +1,93 @@
 #!/bin/bash
 
-## telegram bot id
-# BOT_ID="XXXX:XXXX" # Telegram botのIDを設定。未使用の場合はコメントアウト。
+# telegram bot id
+# BOT_ID="XXXX:XXXX"
 
-DIR=`dirname $0` # 現在のスクリプトのディレクトリを取得。
+DIR=`dirname $0`
 
-cd ${DIR} # スクリプトのディレクトリに移動。
+cd ${DIR}
 
-# 必要なディレクトリが存在しなければ作成。
 test -d ./FILE || mkdir ./FILE
 test -d ./DSC || mkdir ./DSC
 
-# 30日前のディレクトリを削除。
-rm -rf ./FILE/$(date -D %Y%m%d -d "$(date +%Y%m%d) - 30 days")*
-rm -rf ./DSC/$(date -D %Y%m%d -d "$(date +%Y%m%d) - 30 days")*
+# delete old dir
+rm -rf ./FILE/`date +%Y%m%d -d'30 day ago'`*
+rm -rf ./DSC/`date +%Y%m%d -d'30 day ago'`*
 
-FILE_DATE=`date +%Y%m%d%H%M` # 現在の日付と時間を取得。
-OLD_FILE_DATE=`ls -ltr ./FILE/ | tail -n1 | awk '{print $9}'` # 最新のファイル日付を取得。
+FILE_DATE=`date +%Y%m%d%H%M`
+OLD_FILE_DATE=`ls -ltr ./FILE/ |tail -n1 |awk '{print $9}'`
 
-# Googleカレンダーから翌日のスケジュールを取得し、整形。
+# get google calender schedule
 python3 ../get_events.py | sed -e 's:<html-blob>::g' -e 's:</html-blob>::g' -e "s:<br>:\n:g" > schedule.txt
 sed -i -E 's@^.*(https?://[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]).*$@\1@' schedule.txt
 
-# 翌日の日付でテキストを作成。
-sed -e "s/DATE/$(date -D %Y-%m-%d -d '$(date +%Y-%m-%d) + 1 days')/" ../reverse.sed > make_reverse.sed
-sed -n "/$(date -D %Y-%m-%d -d '$(date +%Y-%m-%d) + 1 days')/,/^$/p" schedule.txt | sed -f make_reverse.sed | sed -e "s:$(date -D %Y-%m-%d -d '$(date +%Y-%m-%d) + 1 days'):$(date -D %m/%d -d '$(date +%m/%d) + 1 days'):" -e "s/~.*//" > make.txt
+# make text
+sed -e "s/DATE/`date +%Y-%m-%d --date tomorrow`/" ../reverse.sed > make_reverse.sed
+sed -n "/`date +%Y-%m-%d --date tomorrow`/,/^$/p" schedule.txt  | sed -f make_reverse.sed | sed -e "s:`date +%Y-%m-%d --date tomorrow`:`date +%m/%d --date tomorrow`:" -e "s/~.*//" > make.txt
 
-# 以前のテキストファイルと比較して変更がなければ終了。
 test -f make.txt.`date +%Y%m%d` && diff make.txt make.txt.`date +%Y%m%d` && exit 1
-test -f make.txt.`date +%Y%m%d` && diff make.txt make.txt.`date +%Y%m%d` | grep "<" || (cp -f make.txt make.txt.`date +%Y%m%d` && exit 2)
+test -f make.txt.`date +%Y%m%d` && diff make.txt make.txt.`date +%Y%m%d` | grep "<" || (cp -f make.txt make.txt.`date +%Y%m%d` && exit 2) 
 
-# ファイルディレクトリを作成。
+# make file
 test -d ./FILE/${FILE_DATE} || mkdir -p ./FILE/${FILE_DATE}
 
-# スプレッドシートからクライアント情報を取得し、整形。
-python3 ../spreadsheet_client.py | sed -e 's/],/\n/g' -e 's/]//g' -e 's/\[//g' -e "s/'//g" -e "s/ //g" > client
+# get client
+python3 ../spreadsheet_client.py |sed -e 's/],/\n/g' -e 's/]//g' -e 's/\[//g' -e "s/'//g" -e "s/ //g" > client
 
-# スプレッドシートからメンバー情報を取得し、整形。
-python3 ../spreadsheet_member.py | sed -e 's/],/\n/g' -e 's/]//g' -e 's/\[//g' -e "s/'//g" -e "s/ //g" > member
+# get member
+python3 ../spreadsheet_member.py |sed -e 's/],/\n/g' -e 's/]//g' -e 's/\[//g' -e "s/'//g" -e "s/ //g" > member
 
-# クライアントとメンバーのIDの重複をチェックし、あればスクリプトを終了。
-cat client | awk -F',' '{print $1}' | sort | uniq -d | grep '[^0-9]' && exit 3
-cat member | awk -F',' '{print $1}' | sort | uniq -d | grep '[^0-9]' && exit 4
+# duplication check
+cat client |awk -F',' '{print $1}' |sort |uniq -d | grep -P "\D" && exit 3
+cat member |awk -F',' '{print $1}' |sort |uniq -d | grep -P "\D" && exit 4
 
-# make.txtの内容を読み込み、ファイルに出力。
 while read line
 do
-  echo $line | grep -q Title && ID=`grep "^\`echo $line | awk -F'　' '{print $1}' | sed -e "s/Title://"\`" member | awk -F',' '{print $2}'`
-  echo $line | grep -q Title && MEM=`grep "^\`echo $line | awk -F'　' '{print $1}' | sed -e "s/Title://"\`" member | awk -F',' '{print $1}'`
-  echo $line | grep -q Title && FILE_NAME=`echo $line | sed -e "s/Title://" -e "s/　/_/g"`
-  echo $line | grep -q Title && grep "`echo $line | awk -F'　' '{print $2}'`" client >> ./FILE/${FILE_DATE}/${ID}_${FILE_NAME}
-  echo $line | grep -q Title || echo $line >> ./FILE/${FILE_DATE}/${ID}_${FILE_NAME}
+echo $line  |grep -q Title && ID=`grep "^\`echo $line |awk -F'　' '{print $1}' |sed -e "s/Title://"\`" member |awk -F',' '{print $2}'`
+echo $line  |grep -q Title && MEM=`grep "^\`echo $line |awk -F'　' '{print $1}' |sed -e "s/Title://"\`" member |awk -F',' '{print $1}'`
+echo $line  |grep -q Title && FILE_NAME=`echo $line |sed -e "s/Title://" -e "s/　/_/g"`
+echo $line  |grep -q Title && grep "`echo $line |awk -F'　' '{print $2}'`" client >> ./FILE/${FILE_DATE}/${ID}_${FILE_NAME}
+echo $line  |grep -q Title || echo $line >> ./FILE/${FILE_DATE}/${ID}_${FILE_NAME}
+
 done < make.txt
 
-# DSCディレクトリを作成。
+
 test -d ./DSC/${FILE_DATE} || mkdir -p ./DSC/${FILE_DATE}
 
-# 各ファイルをチェックし、差分があればテレグラムへの投稿準備。
-for i in `ls -tr ./FILE/${FILE_DATE} | grep -v "キャンセル"`
+# post telegram
+for i in `ls -tr ./FILE/${FILE_DATE} |grep -v "キャンセル"`
 do
-  diff ./FILE/${FILE_DATE}/$i ./FILE/${OLD_FILE_DATE}/$i && continue
 
-  ID=`echo $i | awk -F'_' '{print $1}'`
-  MEM=`echo $i | awk -F'_' '{print $2}'`
+diff ./FILE/${FILE_DATE}/$i ./FILE/${OLD_FILE_DATE}/$i && continue
 
-  DATE=`tail -n1 ./FILE/${FILE_DATE}/$i`
-  CLI=`head -n1 ./FILE/${FILE_DATE}/$i | awk -F',' '{print $2" 様  "$3"  "$4"\\\\n"$5}'`
+ID=`echo $i |awk -F'_' '{print $1}'`
+MEM=`echo $i |awk -F'_' '{print $2}'`
 
-  TXT=`cat ./FILE/${FILE_DATE}/$i | sed -e '1d' -e '$d' -e 's/$/ \\\\n/'`
+DATE=`tail -n1 ./FILE/${FILE_DATE}/$i`
+CLI=`head -n1 ./FILE/${FILE_DATE}/$i|awk -F',' '{print $2" 様  "$3"  "$4"\\\\n"$5}'`
 
-  echo "
+TXT=`cat ./FILE/${FILE_DATE}/$i | sed -e '1d' -e '$d' -e 's/$/ \\\\n/'`
+
+echo "
 $DATE \\n
 $TXT \\n
 $CLI \\n
 \\n
 \\n" >> ./DSC/${FILE_DATE}/${ID}_${MEM}
+
 done
 
-# DSCフォルダの各ファイルに対してTelegram APIを使ってメッセージを送信。
 for i in `ls ./DSC/${FILE_DATE}`
 do
-  ID=`echo $i | awk -F'_' '{print $1}'`
-  MEM=`echo $i | awk -F'_' '{print $2}'`
 
-  grep "not found" ./DSC/${FILE_DATE}/$i && sed -i -e "s/not found/場所確認中\\\\n※場所が決まりましたらご連絡致します/" ./DSC/${FILE_DATE}/$i
+ID=`echo $i |awk -F'_' '{print $1}'`
+MEM=`echo $i |awk -F'_' '{print $2}'`
 
-  TXT=`cat ./DSC/${FILE_DATE}/$i`
+grep "not found" ./DSC/${FILE_DATE}/$i && sed -i -e "s/not found/場所確認中\\\\n※場所が決まりましたらご連絡致します/" ./DSC/${FILE_DATE}/$i
 
-  DSC="
+TXT=`cat ./DSC/${FILE_DATE}/$i`
+
+DSC="
 $MEM さん \\n
 前日確認です。 \\n
 \\n
@@ -95,22 +95,24 @@ $TXT
 ご確認よろしくお願い致します。
 "
 
-  curl -s -H 'Accept: application/json' -H "Content-type: application/json" -X POST "https://api.telegram.org/bot${BOT_ID}/sendMessage?chat_id=${ID}" -k -d @- <<EOF
+curl -s  -H 'Accept: application/json' -H "Content-type: application/json" -X POST "https://api.telegram.org/bot${BOT_ID}/sendMessage?chat_id=${ID}" -k -d @- <<EOF
 {
     "text": "${DSC}"
 }
 EOF
 
-  echo ""
+echo ""
+
 done
 
-# make.txtを保存し、前日のファイルを削除。
-cp -f make.txt make.txt.`date +%Y%m%d`
-test -f make.txt.$(date -D %Y%m%d -d "$(date +%Y%m%d) - 1 days") && rm -f make.txt.$(date -D %Y%m%d -d "$(date +%Y%m%d) - 1 days")
+# rm -rf ./FILE/`date +%Y%m%d -d'30 day ago'`*
+# rm -rf ./DSC/`date +%Y%m%d -d'30 day ago'`*
 
-# 一時ファイルを削除。
+cp -f make.txt make.txt.`date +%Y%m%d`
+
+test -f make.txt.`date +%Y%m%d -d'1 day ago'` && rm -f make.txt.`date +%Y%m%d -d'1 day ago'`
+
 rm -f ./client
 rm -f ./member
 
-# スクリプトを正常終了。
 exit 0
